d2adfe48ed0f9f93beb7df7159768b16f621e2b5
Adebambo Adenuga <35952059+Pils36@users.noreply.github.com>
2022-07-04 13:31:18 +0100

    profile page

--- src/components/pages/main/BookSession.jsx
+++ src/components/pages/main/BookSession.jsx
@@ -13,11 +13,12 @@ function BookSession() {
                 <div className="card mb-5">
 
                     <div className="card-body" style={{ paddingBottom: '100px' }}>
-                        <div className="col-md-2">
-                            <i className="fa fa-arrow"></i>
-                        </div>
-                        <div className="col-md-10">
-                            <h5 className="text-white mb-5 pt-4" style={{ fontSize: '25px', fontWeight: '600' }}>Book A Session</h5>
+                        <div className="row mt-5">
+
+                            <div className="col-md-12">
+
+                                <h5 className="text-white" style={{ fontSize: '25px', fontWeight: '600' }}><i class="fa fa-arrow-left" aria-hidden="true" style={{ color: '#FD9B8A' }}></i> Book A Session</h5>
+                            </div>
                         </div>
 
 
